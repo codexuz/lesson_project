9d9f998ee2eaa515e2ce17db9e52912e475ea36f
codexuz <142141576+codexuz@users.noreply.github.com>
2024-05-20 16:31:14 +0500

    Update index.js

--- gtp/index.js
+++ gtp/index.js
@@ -1,20 +1,26 @@
-const express = require('express');
+import express  from 'express';
 const app = express()
-const cors = require('cors')
+import cors  from 'cors'
+import OpenAI   from 'openai'
 
 
 const ApiKey = "sk-proj-G6lWv8gAxQ3ZN0UqT4rrT3BlbkFJkx990Q2tEj9owqpeEadx"
 
+const openai = new OpenAI({
+  apiKey: ApiKey
+})
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
 
-app.get('/', (req, res)=>{
-  res.status(200).json({
-    ism: "Javlon"
-  })
+app.get('/', async (req, res)=>{
+  const completion = await openai.completions.create({
+    model: 'gpt-3.5-turbo-instruct',
+    prompt: 'Write a tagline for an ice cream shop.'
+});
+  res.status(200).json(completion)
 })
 
 const port = 3000;
