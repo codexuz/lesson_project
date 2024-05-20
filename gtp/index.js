602e815b4b42cace713c01d3efdef4aaa2be1651
codexuz <142141576+codexuz@users.noreply.github.com>
2024-05-20 15:35:19 +0500

    Update index.js

--- gtp/index.js
+++ gtp/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 
+
+const ApiKey = "sk-proj-G6lWv8gAxQ3ZN0UqT4rrT3BlbkFJkx990Q2tEj9owqpeEadx"
+
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+
+
 app.get('/', (req, res)=>{
   res.status(200).json({
     ism: "Javlon"
