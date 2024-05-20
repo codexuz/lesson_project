1f66fd7bf829db6166b6205db873dc04015f84ba
codexuz <142141576+codexuz@users.noreply.github.com>
2024-05-20 16:53:48 +0500

    Update index.js

--- gtp/index.js
+++ gtp/index.js
@@ -10,8 +10,31 @@ const openai = new OpenAI({
   apiKey: ApiKey
 })
 
+const allowedOrigins = [
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:5173',
+  'http://localhost:8100',
+  'https://examonline-rouge.vercel.app',
+  'https://examonline-edumo.uz/'
+];
+
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+};
+
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({extended: true}));
 
 
