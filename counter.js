3548b5540bedd33b3c5903afa35a389a5d0a3db4
codexuz <142141576+codexuz@users.noreply.github.com>
2024-07-03 09:54:33 +0500

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,29 @@
+            let deadline = new Date("Jan 5, 2025 15:37:25").getTime();
+ 
+            // To call defined fuction every second
+            let x = setInterval(function () {
+                 
+                // Getting current time in required format
+                let now = new Date().getTime();
+ 
+                // Calculating the difference
+                let t = deadline - now;
+ 
+                // Getting value of days, hours, minutes, seconds
+                let days = Math.floor(t / (1000 * 60 * 60 * 24));
+                let hours = Math.floor(
+                    (t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                let minutes = Math.floor(
+                    (t % (1000 * 60 * 60)) / (1000 * 60));
+                let seconds = Math.floor(
+                    (t % (1000 * 60)) / 1000);
+ 
+                // Output the remaining time
+                document.getElementById("demo").innerHTML =days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+ 
+                // Output for over time
+                if (t < 0) {
+                    clearInterval(x);
+                    document.getElementById("demo").innerHTML = "EXPIRED";
+                }
+            }, 1000);
